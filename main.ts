c6bea24610cfa9145960abfcea29d0485a1b1224
fishiversity <41026299+fishiversity@users.noreply.github.com>
2023-04-24 14:51:19 -0400

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,17 @@
 radio.onReceivedNumber(function (receivedNumber) {
+    comment.comment("When a number is received, store it as \"v\"")
     v = receivedNumber
 })
+function stop () {
+    wuKong.stopAllMotor()
+}
+function forward () {
+    wuKong.setAllMotor(100, 100)
+}
 let v = 0
+comment.comment("Tell the Micro:Bit it is attached to the Joystick")
 joystickbit.initJoystickBit()
+comment.comment("Specify the frequency for your set")
 radio.setGroup(2)
 basic.showLeds(`
     # . . . .
@@ -12,24 +21,21 @@ basic.showLeds(`
     # # # # #
     `)
 basic.forever(function () {
-    if (joystickbit.getRockerValue(joystickbit.rockerType.X) < 200) {
-        radio.sendNumber(4)
-    } else if (joystickbit.getRockerValue(joystickbit.rockerType.X) > 800) {
-        radio.sendNumber(3)
-    } else if (joystickbit.getRockerValue(joystickbit.rockerType.Y) < 200) {
-        radio.sendNumber(2)
-    } else if (joystickbit.getRockerValue(joystickbit.rockerType.Y) > 800) {
+    if (joystickbit.getRockerValue(joystickbit.rockerType.Y) > 800) {
+        comment.comment("When the joystick moves forward, send the number 1")
         radio.sendNumber(1)
-    } else if (joystickbit.getButton(joystickbit.JoystickBitPin.P12)) {
-        radio.sendNumber(5)
-    } else if (joystickbit.getButton(joystickbit.JoystickBitPin.P13)) {
-        radio.sendNumber(6)
-    } else if (!(joystickbit.getButton(joystickbit.JoystickBitPin.P13))) {
-        radio.sendNumber(0)
-    } else if (!(joystickbit.getButton(joystickbit.JoystickBitPin.P12))) {
-        radio.sendNumber(0)
     } else {
+        comment.comment("When the joystick does not move, send the number 0")
         radio.sendNumber(0)
+    }
+})
+basic.forever(function () {
+    if (v == 1) {
+        comment.comment("If the received number is 1, go forward")
+        forward()
+    } else if (v == 0) {
+        comment.comment("If the received number is 0, stop")
+        stop()
     }
 })
 basic.forever(function () {
